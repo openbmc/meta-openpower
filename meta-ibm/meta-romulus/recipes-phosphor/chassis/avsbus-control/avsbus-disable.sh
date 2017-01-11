openbmc/openbmc

meta-romulus: Add AVSBus and VRM workarounds
Add AVSBus and VRM control scripts.
Add systemd services to do AVSBus workaround, disable AVSBus, do VRM workaround and enable
AVSBus.
Add BitBake recipe to install script and services.

Change-Id: I72ec0138d23eba57ee56d5a6b01e3cd638c819bc
Signed-off-by: Lei YU <mine260309@gmail.com>

meta-openbmc-machines/meta-openpower/meta-ibm/meta-romulus/recipes-phosphor/chassis/avsbus-control/avsbus-disable.sh
@@ -1,0 +1,12 @@
+#!/bin/sh
+
+i2cset -y 4 0x71 0x01 0x80 b    # VDD 0
+i2cset -y 4 0x71 0x00 0x01 b    # VCS 0  - PAGE set
+i2cset -y 4 0x71 0x01 0x80 b    # VCS 0
+i2cset -y 4 0x71 0x00 0x00 b    # VCS 0  - PAGE reset
+i2cset -y 4 0x72 0x01 0x80 b    # VDN 0
+i2cset -y 5 0x71 0x01 0x80 b    # VDD 1
+i2cset -y 5 0x71 0x00 0x01 b    # VCS 1  - PAGE set
+i2cset -y 5 0x71 0x01 0x80 b    # VCS 1
+i2cset -y 5 0x71 0x00 0x00 b    # VCS 1  - PAGE reset
+i2cset -y 5 0x72 0x01 0x80 b    # VDN 1

meta-openbmc-machines/meta-openpower/meta-ibm/meta-romulus/recipes-phosphor/chassis/avsbus-control/avsbus-enable.sh
@@ -1,0 +1,12 @@
+#!/bin/sh
+
+i2cset -y 4 0x71 0x01 0xB0 b    # VDD 0
+i2cset -y 4 0x71 0x00 0x01 b    # VCS 0  - PAGE set
+i2cset -y 4 0x71 0x01 0xB0 b    # VCS 0
+i2cset -y 4 0x71 0x00 0x00 b    # VCS 0  - PAGE reset
+i2cset -y 4 0x72 0x01 0xB0 b    # VDN 0
+i2cset -y 5 0x71 0x01 0xB0 b    # VDD 1
+i2cset -y 5 0x71 0x00 0x01 b    # VCS 1  - PAGE set
+i2cset -y 5 0x71 0x01 0xB0 b    # VCS 1
+i2cset -y 5 0x71 0x00 0x00 b    # VCS 1  - PAGE reset
+i2cset -y 5 0x72 0x01 0xB0 b    # VDN 1

meta-openbmc-machines/meta-openpower/meta-ibm/meta-romulus/recipes-phosphor/chassis/avsbus-control/avsbus-workaround.sh
@@ -1,0 +1,7 @@
+#!/bin/sh
+
+i2cset -y 4 0x14 0x2E 0x23 b # VDD/VCS 0
+i2cset -y 4 0x15 0x2E 0x23 b # VDN 0
+i2cset -y 5 0x14 0x2E 0x23 b # VDD/VCS 1
+i2cset -y 5 0x14 0x2E 0x23 b # VDN 1
+

openbmc/meta-openpower

meta-ibm/meta-romulus/recipes-phosphor/chassis/avsbus-control/avsbus-disable.sh
@@ -1,0 +1,12 @@
+#!/bin/sh
+
+i2cset -y 4 0x70 0x01 0x80 b    # VDD 0
+i2cset -y 4 0x70 0x00 0x01 b    # VCS 0  - PAGE set
+i2cset -y 4 0x70 0x01 0x80 b    # VCS 0
+i2cset -y 4 0x70 0x00 0x00 b    # VCS 0  - PAGE reset
+i2cset -y 4 0x73 0x01 0x80 b    # VDN 0
+i2cset -y 5 0x70 0x01 0x80 b    # VDD 1
+i2cset -y 5 0x70 0x00 0x01 b    # VCS 1  - PAGE set
+i2cset -y 5 0x70 0x01 0x80 b    # VCS 1
+i2cset -y 5 0x70 0x00 0x00 b    # VCS 1  - PAGE reset
+i2cset -y 5 0x73 0x01 0x80 b    # VDN 1

meta-ibm/meta-romulus/recipes-phosphor/chassis/avsbus-control/avsbus-enable.sh
@@ -1,0 +1,12 @@
+#!/bin/sh
+
+i2cset -y 4 0x70 0x01 0xB0 b    # VDD 0
+i2cset -y 4 0x70 0x00 0x01 b    # VCS 0  - PAGE set
+i2cset -y 4 0x70 0x01 0xB0 b    # VCS 0
+i2cset -y 4 0x70 0x00 0x00 b    # VCS 0  - PAGE reset
+i2cset -y 4 0x73 0x01 0xB0 b    # VDN 0
+i2cset -y 5 0x70 0x01 0xB0 b    # VDD 1
+i2cset -y 5 0x70 0x00 0x01 b    # VCS 1  - PAGE set
+i2cset -y 5 0x70 0x01 0xB0 b    # VCS 1
+i2cset -y 5 0x70 0x00 0x00 b    # VCS 1  - PAGE reset
+i2cset -y 5 0x73 0x01 0xB0 b    # VDN 1

meta-ibm/meta-romulus/recipes-phosphor/chassis/avsbus-control/avsbus-workaround.sh
@@ -1,0 +1,7 @@
+#!/bin/sh
+
+i2cset -y 4 0x28 0x2E 0x23 b # VDD/VCS 0
+i2cset -y 4 0x2B 0x2E 0x23 b # VDN 0
+i2cset -y 5 0x28 0x2E 0x23 b # VDD/VCS 1
+i2cset -y 5 0x2B 0x2E 0x23 b # VDN 1
+