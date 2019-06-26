f9a45e399407a1f599cfd0d8c389eabbc9127d62
asmithakarun <asmithkr@in.ibm.com>
2019-06-26 02:04:53 -0500

    Avahi: Appending machine-id to the hostname when fetching of the Serial Number fails
    The idea of this commit is to handle situations where the
    serial number is not written into bmc-vpd. In such cases,
    the machine-id will be appended with the hostname to make
    the hostname unique.
    
    Signed-off-by: asmithakarun <asmithkr@in.ibm.com>
    Change-Id: I821c5b1e7943cc17001dc1e613cca9b48c58375f

--- recipes-phosphor/network/first-boot-set-hostname/first-boot-set-hostname.sh
+++ recipes-phosphor/network/first-boot-set-hostname/first-boot-set-hostname.sh
@@ -31,7 +31,7 @@ sync_hostname() {
 
     if [[ -z "${BMC_ITEM_SERVICE}" ]]; then
         show_error "No BMC item found in the Inventory. Is VPD EEPROM empty?"
-        return
+        BMC_ITEM_SERV_EMPTY=1
     fi
 
     BMC_SN=$(busctl get-property ${BMC_ITEM_SERVICE} \
@@ -41,7 +41,14 @@ sync_hostname() {
     BMC_SN=${BMC_SN#*\"}
     BMC_SN=${BMC_SN%\"*}
 
-    hostnamectl set-hostname {MACHINE}-${BMC_SN}
+    if [[ -z "${BMC_SN}" ]]  || [ $BMC_ITEM_SERV_EMPTY == 1 ]; then
+        echo "Setting Hostname as 'hostname + machine-id' "
+        MACHINE_ID=`cat /etc/machine-id`
+        hostnamectl set-hostname {MACHINE}-${MACHINE_ID}
+    else
+        hostnamectl set-hostname {MACHINE}-${BMC_SN}
+    fi
+
 }
 
 [ "$(hostname)" = "{MACHINE}" ] && sync_hostname
