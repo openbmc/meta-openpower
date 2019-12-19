91267c3b0b403e981b6f17af15555fbcc407a3d5
George Liu <liuxiwei@inspur.com>
2019-12-19 19:12:32 +0800

    Add http-redirection recipes
    Use nc command to listen to WebUI 80 port and implement redirect HTTP
    to HTTPS.
    
    Tested: Visit `http://${ip}` and redirect to `https://${ip}`.
    
    Signed-off-by: George Liu <liuxiwei@inspur.com>
    Change-Id: If701f5199fefbe161c88b6b88d8650a657d2175c

---
+++ recipes-phosphor/http-redirection/http-redirection/http-redirection.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+bmcIp=""
+bmcPath="/"
+
+readRequest() {
+	while true
+	do
+		read line<&${COPROC[0]}
+		reqKey=`echo $line | awk '{print$1}' | awk -F : '{print$1}'`
+
+		if [ -z "$reqKey" ]; then
+			break
+		fi
+
+		if [ "${reqKey}" == "GET" ]; then
+			bmcPath=`echo $line | awk '{print$2}'`
+		fi
+
+		if [ "${reqKey}" == "Host" ]; then
+			bmcIp=`echo $line | awk -F : '{print$2}' | awk '{gsub(/^\s+|\s+$/, "");print}'`
+		fi
+	done
+
+	if [ -z "$bmcIp" ]; then
+		exit -2
+	fi
+}
+
+sendRespData() {
+	echo >&${COPROC[1]} "HTTP/1.1 308 Permanent Redirect"
+	echo >&${COPROC[1]} "Location: https://$bmcIp$bmcPath"
+	echo >&${COPROC[1]} "Content-Type: text/html; charset=UTF-8"
+	echo >&${COPROC[1]} "Strict-Transport-Security:max-age=31536000;includeSubDomains"
+}
+
+main() {
+	while true
+	do
+		coproc ( nc -l -p 80 )
+		{
+			readRequest
+			sendRespData
+		}
+		kill "$COPROC_PID"
+		sleep 0.1
+	done
+}
+
+main
